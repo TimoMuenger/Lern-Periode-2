88035048bc393610cadfc5c2a6979fe18a803d6d
TimoMuenger <t.muenger.inf24@stud.bbbaden.ch>
2024-11-01 11:45:50 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,45 +1,31 @@
-﻿using System.ComponentModel.Design;
+﻿using System.Runtime.InteropServices;
 
-namespace Lern_Periode_2
+namespace demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string[] field = { "x", "_", "_", "_", "_" };
+            foreach(string f in field)
+            {
+                Console.Write(f);
+            }
+
             
+            Console.Clear();
+            field[0] = "x";
+            field[1] = "_";
+            foreach (string f in field)
+            {
+                Console.Write(f);
+            }
+            Console.Clear();
+            field[1] = "_";
+            field[2] = "x";
 
-                Random zufall = new Random();
-                int zufallszahl = zufall.Next(1, 100);
-               
-                
-
-                Console.WriteLine("Eraten sie eine Zahl zwischen 1 und 100");
-               
-                int guess = int.Parse(Console.ReadLine());
-               
-                if (guess > zufallszahl)
-                {
-                    Console.WriteLine("Die eingegebene Zahl ist zu gross");
-
-                }
-                
-                else if (guess < zufallszahl) 
-                {
-                    Console.WriteLine("Die eingegebene Zahl ist zu klein");
-
-                }
-                else if (guess == zufallszahl)
-                {
-                    Console.WriteLine("Sie haben die Zahl erraten und gewonnen!");
-
-                }
-
-
-            while (guess != zufallszahl);
-
-        
         }
-            
+    
     }
-
 }
+
