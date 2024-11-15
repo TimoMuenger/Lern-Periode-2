9879a4991f586c982660d1ab2d86603f5684c811
TimoMuenger <t.muenger.inf24@stud.bbbaden.ch>
2024-11-15 11:27:57 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,74 @@
-﻿using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace demo
+namespace Chat_GPT_Beispiel
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
-        {
-            string[] field = { "x", "_", "_", "_", "_" };
-            foreach(string f in field)
-            {
-                Console.Write(f);
-            }
-
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        { }
             
-            Console.Clear();
-            field[0] = "x";
-            field[1] = "_";
-            foreach (string f in field)
-            {
-                Console.Write(f);
-            }
-            Console.Clear();
-            field[1] = "_";
-            field[2] = "x";
+         
+public class BewegungsForm : Form
+        {
+            private int x = 50; // Startposition X
+            private int y = 50; // Startposition Y
+            private int schrittweite = 10; // Anzahl der Pixel für jeden Schritt
 
+            public BewegungsForm()
+            {
+                this.Text = "Bewegung in 2D";
+                this.Size = new Size(800, 600);
+                this.DoubleBuffered = true; // Reduziert Flackern beim Neuzeichnen
+                this.KeyDown += new KeyEventHandler(OnKeyDown); // Event-Handler für Tastatur-Eingaben
+            }
+
+            // Diese Methode wird aufgerufen, wenn eine Taste gedrückt wird
+            private void OnKeyDown(object sender, KeyEventArgs e)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Up:
+                        y -= schrittweite;
+                        break;
+                    case Keys.Down:
+                        y += schrittweite;
+                        break;
+                    case Keys.Left:
+                        x -= schrittweite;
+                        break;
+                    case Keys.Right:
+                        x += schrittweite;
+                        break;
+                }
+                Invalidate(); // Löst die Neuzeichnung aus
+            }
+
+            // Zeichnet das bewegte Objekt (Rechteck)
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                Graphics g = e.Graphics;
+                g.Clear(Color.White); // Hintergrundfarbe der Fläche
+                g.FillRectangle(Brushes.Blue, x, y, 50, 50); // Zeichnet ein blaues Rechteck an Position (x, y) mit Größe 50x50
+            }
+
+            // Startet das Formular
+            [STAThread]
+            public static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.Run(new BewegungsForm());
+            }
         }
-    
+
     }
 }
 
