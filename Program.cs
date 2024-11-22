2915e9cad2928bf6dd0f7bcc85f0d3aa011bdab6
TimoMuenger <t.muenger.inf24@stud.bbbaden.ch>
2024-11-22 11:45:11 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,74 +1,27 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Globalization;
+using System.Net.WebSockets;
 
-namespace Chat_GPT_Beispiel
+namespace VociTrainerEnglisch
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        { }
-            
-         
-public class BewegungsForm : Form
+        static void Main(string[] args)
         {
-            private int x = 50; // Startposition X
-            private int y = 50; // Startposition Y
-            private int schrittweite = 10; // Anzahl der Pixel für jeden Schritt
+            string[] tiere = { "Hund", "Katze", "Pferd", "Elefant", "Löwe", "Tiger", "Hase", "Fuchs", "Bär", "Giraffe" };
+            string[] animals = { "Dog", "Cat", "Horse", "Elephant", "Lion", "Tiger", "Rabbit", "Fox", "Bear", "Giraffe" };
+            String answerUser;
 
-            public BewegungsForm()
+            Console.WriteLine("Willkommen zum Voci Trainer.");
+            Console.WriteLine("Was heisst" tiere[0] "auf Englisch");
+            answerUser = Console.ReadLine();
+            if (answerUser == animals[0])
             {
-                this.Text = "Bewegung in 2D";
-                this.Size = new Size(800, 600);
-                this.DoubleBuffered = true; // Reduziert Flackern beim Neuzeichnen
-                this.KeyDown += new KeyEventHandler(OnKeyDown); // Event-Handler für Tastatur-Eingaben
+                Console.WriteLine("Richtige Antwort!"); 
             }
-
-            // Diese Methode wird aufgerufen, wenn eine Taste gedrückt wird
-            private void OnKeyDown(object sender, KeyEventArgs e)
+            else
             {
-                switch (e.KeyCode)
-                {
-                    case Keys.Up:
-                        y -= schrittweite;
-                        break;
-                    case Keys.Down:
-                        y += schrittweite;
-                        break;
-                    case Keys.Left:
-                        x -= schrittweite;
-                        break;
-                    case Keys.Right:
-                        x += schrittweite;
-                        break;
-                }
-                Invalidate(); // Löst die Neuzeichnung aus
-            }
-
-            // Zeichnet das bewegte Objekt (Rechteck)
-            protected override void OnPaint(PaintEventArgs e)
-            {
-                Graphics g = e.Graphics;
-                g.Clear(Color.White); // Hintergrundfarbe der Fläche
-                g.FillRectangle(Brushes.Blue, x, y, 50, 50); // Zeichnet ein blaues Rechteck an Position (x, y) mit Größe 50x50
-            }
-
-            // Startet das Formular
-            [STAThread]
-            public static void Main()
-            {
-                Application.EnableVisualStyles();
-                Application.Run(new BewegungsForm());
+                Console.WriteLine("Falsche Antwort");
             }
         }
-
     }
 }
-
