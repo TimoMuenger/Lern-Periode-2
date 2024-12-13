bfd14d178610226f9468fa8f7fa521cc8c792a91
TimoMuenger <t.muenger.inf24@stud.bbbaden.ch>
2024-12-13 11:45:38 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,148 +1,56 @@
-using System.Globalization;
-using System.Net.WebSockets;
-
-namespace VociTrainerEnglisch
-{
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] tiere = { "Hund", "Katze", "Pferd", "Elefant", "Löwe", "Tiger", "Hase", "Fuchs", "Bär", "Giraffe" };
             string[] animals = { "Dog", "Cat", "Horse", "Elephant", "Lion", "Tiger", "Rabbit", "Fox", "Bear", "Giraffe" };
-            String answerUser;
+            string[] animaux = { "Chien", "Chat", "Cheval", "Éléphant", "Lion", "Tigre", "Lièvre", "Renard", "Ours", "Girafe" };
 
-            { Console.WriteLine("Willkommen zum Voci Trainer.");
-                Console.WriteLine("Was heisst:" tiere[0] "auf Englisch");
-                answerUser = Console.ReadLine();
-                if (answerUser == animals[0])
-                {
-                    Console.WriteLine("Richtige Antwort!");
-                }
-                else
-                {
-                    Console.WriteLine("Falsche Antwort");
-                }
+            string LanguageUserAnswer;
+            string AnimalUserAnswer;
 
-                { Console.WriteLine("Was heisst:" tiere[1] "auf Englisch");
-                    answerUser = Console.ReadLine();
-                    if (answerUser == animals[1])
+            Console.WriteLine("Wollen sie Englisch oder Französisch lernen?");
+            LanguageUserAnswer = Console.ReadLine();
+
+            for (int i = 0; i < tiere.Length; i++)
+            {
+
+
+
+
+                if (LanguageUserAnswer == "Englisch")
+                {
+                    Console.WriteLine("Was heisst " + tiere[i] + " auf Englisch?");
+                    AnimalUserAnswer = Console.ReadLine();
+                    if (AnimalUserAnswer.ToLower() == animals[i].ToLower())
                     {
                         Console.WriteLine("Richtige Antwort!");
                     }
                     else
                     {
-                        Console.WriteLine("Falsche Antwort");
+                        Console.WriteLine("Falsche Antwort!");
+
                     }
-                    { Console.WriteLine("Was heisst:" tiere[2] "auf Englisch");
-                        answerUser = Console.ReadLine();
-                        if (answerUser == animals[2])
-                        {
-                            Console.WriteLine("Richtige Antwort!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Falsche Antwort");
-                        }
-                        { Console.WriteLine("Was heisst:" tiere[3] "auf Englisch");
-                            answerUser = Console.ReadLine();
-                            if (answerUser == animals[3])
-                            {
-                                Console.WriteLine("Richtige Antwort!");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Falsche Antwort");
-                            }
-                            { Console.WriteLine("Was heisst:" tiere[4] "auf Englisch");
-                                answerUser = Console.ReadLine();
-                                if (answerUser == animals[4])
-                                {
-                                    Console.WriteLine("Richtige Antwort!");
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Falsche Antwort");
-                                }
-                                { Console.WriteLine("Was heisst:" tiere[5] "auf Englisch");
-                                    answerUser = Console.ReadLine();
-                                    if (answerUser == animals[5])
-                                    {
-                                        Console.WriteLine("Richtige Antwort!");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine("Falsche Antwort");
-                                    }
-                                    { Console.WriteLine("Was heisst:" tiere[6] "auf Englisch");
-                                        answerUser = Console.ReadLine();
-                                        if (answerUser == animals[6])
-                                        {
-                                            Console.WriteLine("Richtige Antwort!");
-                                        }
-                                        else
-                                        {
-                                            Console.WriteLine("Falsche Antwort");
-                                        }
-                                        { Console.WriteLine("Was heisst:" tiere[6] "auf Englisch");
-                                            answerUser = Console.ReadLine();
-                                            if (answerUser == animals[6])
-                                            {
-                                                Console.WriteLine("Richtige Antwort!");
-                                            }
-                                            else
-                                            {
-                                                Console.WriteLine("Falsche Antwort");
-                                            }
-                                            { Console.WriteLine("Was heisst:" tiere[7] "auf Englisch");
-                                                answerUser = Console.ReadLine();
-                                                if (answerUser == animals[7])
-                                                {
-                                                    Console.WriteLine("Richtige Antwort!");
-                                                }
-                                                else
-                                                {
-                                                    Console.WriteLine("Falsche Antwort");
-                                                }
-                                                { Console.WriteLine("Was heisst:" tiere[8] "auf Englisch");
-                                                    answerUser = Console.ReadLine();
-                                                    if (answerUser == animals[8])
-                                                    {
-                                                        Console.WriteLine("Richtige Antwort!");
-                                                    }
-                                                    else
-                                                    {
-                                                        Console.WriteLine("Falsche Antwort");
-                                                    }
-                                                    { Console.WriteLine("Was heisst:" tiere[9] "auf Englisch");
-                                                        answerUser = Console.ReadLine();
-                                                        if (answerUser == animals[9])
-                                                        {
-                                                            Console.WriteLine("Richtige Antwort!");
-                                                        }
-                                                        else
-                                                        {
-                                                            Console.WriteLine("Falsche Antwort");
-                                                        }
 
+                }
 
-
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                if (LanguageUserAnswer == "Französisch")
+                {
+                    Console.WriteLine("Was heisst " + tiere[i] + " auf Französisch?");
+                    AnimalUserAnswer = Console.ReadLine();
+                    if (AnimalUserAnswer.ToLower() == animaux[i].ToLower())
+                    {
+                        Console.WriteLine("Richtige Antwort!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Falsche Antwort!");
                     }
                 }
             }
         }
     }
 }
-
-                        
-                            
 
 
               
